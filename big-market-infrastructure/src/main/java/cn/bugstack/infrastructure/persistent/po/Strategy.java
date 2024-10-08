deaf5c2b1342b2b4bb9054335195659411e6f107
yangbo <972849883@qq.com>
2024-10-08 17:54:41 +0800

    策略装配,全部装起来

--- big-market-infrastructure/src/main/java/cn/bugstack/infrastructure/persistent/po/Strategy.java
+++ big-market-infrastructure/src/main/java/cn/bugstack/infrastructure/persistent/po/Strategy.java
@@ -18,6 +18,9 @@ public class Strategy {
     private Long strategyId;
     /** 抽奖策略描述 */
     private String strategyDesc;
+    /** 抽奖规则模型 */
+    private String ruleModels;
+
     /** 创建时间 */
     private Date createTime;
     /** 更新时间 */
