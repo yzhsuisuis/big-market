457c511a2d70eaab7ce3ef20973c4d7d8ae4d5ad
yangbo <972849883@qq.com>
2024-10-13 09:21:27 +0800

    第89章基本完结.

--- big-market-domain/src/main/java/cn/bugstack/domain/strategy/service/rule/filter/factory/DefaultLogicFactory.java
+++ big-market-domain/src/main/java/cn/bugstack/domain/strategy/service/rule/filter/factory/DefaultLogicFactory.java
@@ -39,6 +39,8 @@ public class DefaultLogicFactory {
     @AllArgsConstructor
     public enum LogicModel {
 
+        RULE_WIGHT("rule_weight", "【抽奖前规则】根据抽奖权重返回可抽奖范围KEY", "before"),
+        RULE_BLACKLIST("rule_blacklist", "【抽奖前规则】黑名单规则过滤，命中黑名单则直接返回", "before"),
         RULE_LOCK("rule_lock", "【抽奖中规则】抽奖n次后，对应奖品可解锁抽奖", "center"),
         RULE_LUCK_AWARD("rule_luck_award", "【抽奖后规则】抽奖n次后，对应奖品可解锁抽奖", "after"),
         ;
