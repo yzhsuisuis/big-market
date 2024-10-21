b6fa3fc14fb6310a39999fa0b08aebedc385baa2
yangbo <972849883@qq.com>
2024-10-21 17:16:46 +0800

    第11节完结

---
+++ big-market-app/src/test/java/cn/bugstack/test/domain/myTest.java
@@ -0,0 +1,13 @@
+package cn.bugstack.test.domain;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class myTest {
+	@Test
+	void test01()
+	{
+
+	}
+}
