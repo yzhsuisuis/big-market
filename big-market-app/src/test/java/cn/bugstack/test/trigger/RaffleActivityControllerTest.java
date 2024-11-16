1ae25273820e65bf2c264b7ee815f177d2f04670
yangbo <972849883@qq.com>
2024-11-16 20:16:26 +0800

    fix: 用户行为返利入账单(成功搞定,记得活动预热)

--- big-market-app/src/test/java/cn/bugstack/test/trigger/RaffleActivityControllerTest.java
+++ big-market-app/src/test/java/cn/bugstack/test/trigger/RaffleActivityControllerTest.java
@@ -1,11 +1,13 @@
 package cn.bugstack.test.trigger;
 
+import cn.bugstack.domain.activity.service.armory.IActivityArmory;
 import cn.bugstack.trigger.api.IRaffleActivityService;
 import cn.bugstack.trigger.api.dto.ActivityDrawRequestDTO;
 import cn.bugstack.trigger.api.dto.ActivityDrawResponseDTO;
 import cn.bugstack.types.model.Response;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,6 +28,12 @@ public class RaffleActivityControllerTest {
 
     @Resource
     private IRaffleActivityService raffleActivityService;
+    @Resource
+    private IActivityArmory activityArmory;
+    @Before
+    public void setUp() {
+        log.info("装配活动：{}", activityArmory.assembleActivitySku(9011L));
+    }
 
     @Test
     public void test_armory() {
@@ -49,7 +57,7 @@ public class RaffleActivityControllerTest {
     public void test_calendarSignRebate () throws InterruptedException {
         Response<Boolean> response = raffleActivityService.calendarSignRebate("xiaofuge");
         log.info("测试结果：{}", JSON.toJSONString(response));
-        new CountDownLatch(1).await();
+//        new CountDownLatch(1).await();
     }
 
 }
