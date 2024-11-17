f92b385d27c1537dcb57d9fcb03c27ba44fc5af8
yangbo <972849883@qq.com>
2024-11-17 12:05:53 +0800

    fix:签到返利真正完成,之前少写了一个字段out_bussiness_NO

--- big-market-domain/src/main/java/cn/bugstack/domain/rebate/model/entity/BehaviorRebateOrderEntity.java
+++ big-market-domain/src/main/java/cn/bugstack/domain/rebate/model/entity/BehaviorRebateOrderEntity.java
@@ -28,6 +28,8 @@ public class BehaviorRebateOrderEntity {
     private String rebateType;
     /** 返利配置【sku值，积分值】 */
     private String rebateConfig;
+    /** 业务仿重ID - 外部透传，方便查询使用 */
+    private String outBusinessNo;
     /** 业务ID - 拼接的唯一值 */
     private String bizId;
 
